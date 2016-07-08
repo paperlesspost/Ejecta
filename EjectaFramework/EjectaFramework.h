1551ffece44fd7c494b2233f012299d973c87379
Wyatt McBain <wyatt@paperlesspost.com>
2016-07-08 15:58:55 -0400

    Framework

---
+++ EjectaFramework/EjectaFramework.h
@@ -0,0 +1,19 @@
+//
+//  EjectaFramework.h
+//  EjectaFramework
+//
+//  Created by Wyatt McBain on 7/8/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EjectaFramework.
+FOUNDATION_EXPORT double EjectaFrameworkVersionNumber;
+
+//! Project version string for EjectaFramework.
+FOUNDATION_EXPORT const unsigned char EjectaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EjectaFramework/PublicHeader.h>
+
+
